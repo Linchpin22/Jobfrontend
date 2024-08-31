1d572e3fc322fce43174e9faf3d688811f7a04cf
Linchpin22 <tiwarianuj712u@gmail.com>
2024-08-31 20:36:59 +0530

    front

---
+++ src/components/Auth/Register.jsx
@@ -0,0 +1,155 @@
+import React, { useContext, useState } from "react";
+import { FaRegUser, FaPencilAlt } from "react-icons/fa";
+import { MdOutlineMailOutline } from "react-icons/md";
+import { RiLock2Fill } from "react-icons/ri";
+import { FaPhoneFlip } from "react-icons/fa6";
+import { Link, Navigate } from "react-router-dom";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { Context } from "../../main";
+
+const Register = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("");
+  const [resume, setResume] = useState(null);
+
+  const { isAuthorized, setIsAuthorized } = useContext(Context);
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    try {
+      const formData = new FormData();
+      formData.append("name", name);
+      formData.append("email", email);
+      formData.append("phone", phone);
+      formData.append("password", password);
+      formData.append("role", role);
+      if (resume) {
+        formData.append("resume", resume);
+      }
+
+      const { data } = await axios.post(
+        "http://localhost:4000/api/v1/user/register",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+          withCredentials: true,
+        }
+      );
+
+      toast.success(data.message);
+      setName("");
+      setEmail("");
+      setPassword("");
+      setPhone("");
+      setRole("");
+      setResume(null);
+      setIsAuthorized(true);
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
+  if (isAuthorized) {
+    return <Navigate to={"/"} />;
+  }
+
+  return (
+    <>
+      <section className="authPage">
+        <div className="container">
+          <div className="header">
+            <h1 className="text-black">YourHr</h1>
+            <h3>Create a new account</h3>
+          </div>
+          <form>
+            <div className="inputTag">
+              <label>Register As</label>
+              <div>
+                <select value={role} onChange={(e) => setRole(e.target.value)}>
+                  <option value="">Select Role</option>
+                  <option value="Employer">Employer</option>
+                  <option value="Job Seeker">Job Seeker</option>
+                </select>
+                <FaRegUser />
+              </div>
+            </div>
+            <div className="inputTag">
+              <label>Name</label>
+              <div>
+                <input
+                  type="text"
+                  placeholder="Anuj"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+                <FaPencilAlt />
+              </div>
+            </div>
+            <div className="inputTag">
+              <label>Email Address</label>
+              <div>
+                <input
+                  type="email"
+                  placeholder="anuj@gmail.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <MdOutlineMailOutline />
+              </div>
+            </div>
+            <div className="inputTag">
+              <label>Phone Number</label>
+              <div>
+                <input
+                  type="number"
+                  placeholder="12345678"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                />
+                <FaPhoneFlip />
+              </div>
+            </div>
+            <div className="inputTag">
+              <label>Password</label>
+              <div>
+                <input
+                  type="password"
+                  placeholder="Your Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <RiLock2Fill />
+              </div>
+            </div>
+            <div className="inputTag">
+              <label>Upload Resume</label>
+              <div>
+                <input
+                  type="file"
+                  accept=".pdf,.doc,.docx"
+                  onChange={(e) => setResume(e.target.files[0])}
+                />
+              </div>
+            </div>
+            <button type="submit" onClick={handleRegister}>
+              Register
+            </button>
+            <Link to={"/login"}>Login Now</Link>
+          </form>
+        </div>
+        <div className="banner">
+          <img src="/register.png" alt="login" />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Register;
